5cacb4a7e130331e36da239166796c260cc78873
goldspide <raoulbonsso@gmail.com>
2025-01-13 08:40:45 +0100

    model creation for project description

---
+++ portfolio/components/Model.js
@@ -0,0 +1,11 @@
+const Model = () => {
+  return (
+    <div className="fixed inset-0 w-full h-screen bg-black/25 p-4 backdrop-blur z-[9999999] flex items-center justify-center">
+      <div className="max-w-screen-xl w-full bg-white dark:bg-zinc-900 p-6 rounded-lg border border-zinc-100 dark:border-zinc-700 max-h-[80vh]">dk,mq,qd,
+
+      </div>
+    </div>
+  );
+};
+
+export default Model;
